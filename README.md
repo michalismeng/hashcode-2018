7b613832a155a30f94f0d2637773ba7b54c57987
michalis megis <megisoglou56@gmail.com>
2018-03-02 21:07:25 +0200

    Final commit.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# This is the hashcode repository team entropy sources
+# Hashcode 2018 - Team Entropy Sources 
 
-The source code submitted is debian.cpp
+### Team members
+- Konstantinos Agiannis
+- Rafael Kourdis
+- Michalis Megisoglou
+- Stavros Filosidis
+
+The source code is submitted in debian.cpp <br>
+To generate the output files, download the input files in the directory and run `tests.sh`
--- tests.sh
+++ tests.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 rm a.out
 g++ -O3 -Wall debian.cpp
-./a.out < ~/Downloads/a_example.in > a_example.out
-./a.out < ~/Downloads/b_should_be_easy.in > b_should_be_easy.out
-./a.out < ~/Downloads/c_no_hurry.in > c_no_hurry.out
-./a.out < ~/Downloads/d_metropolis.in > d_metropolis.out
-./a.out < ~/Downloads/e_high_bonus.in > e_high_bonus.out
-
+./a.out < a_example.in > a_example.out
+./a.out < b_should_be_easy.in > b_should_be_easy.out
+./a.out < c_no_hurry.in > c_no_hurry.out
+./a.out < d_metropolis.in > d_metropolis.out
+./a.out < e_high_bonus.in > e_high_bonus.out
